452210a2056c74e3b84890301ed7c11421544058
hdeg <76611318+exit1991@users.noreply.github.com>
2025-07-15 21:13:33 +0900

    サイドバーのアイコンのサイズを調整

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,20 +6,20 @@ export function Sidebar() {
   return (
     <aside className="w-64 h-screen bg-gray-100 p-4">
       <div className="flex items-center mb-8 space-x-2">
-        <FontAwesomeIcon icon={faBook} />
+        <FontAwesomeIcon icon={faBook} className="w-10 h-10" />
         <span className="font-bold">kakibo-app</span>
       </div>
       <nav className="flex flex-col space-y-4">
         <Link href="/" className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faHome} />
+          <FontAwesomeIcon icon={faHome} className="w-6 h-6" />
           <span>トップ</span>
         </Link>
         <Link href="/list" className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faBook} />
+          <FontAwesomeIcon icon={faBook} className="w-6 h-6" />
           <span>家計簿一覧</span>
         </Link>
         <Link href="/new" className="flex items-center space-x-2">
-          <FontAwesomeIcon icon={faPlus} />
+          <FontAwesomeIcon icon={faPlus} className="w-6 h-6" />
           <span>新規登録</span>
         </Link>
       </nav>
